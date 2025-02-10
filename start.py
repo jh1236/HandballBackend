ac4e09b27828f8c87d5690c813356b83678cf852
healy <healy_jared@yahoo.com>
2025-02-10 14:04:46 +0800

    hopefully error handling

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import flask
 from jinja2.ext import debug
+from werkzeug.exceptions import InternalServerError
 
 from database import db
 from utils.args_handler import args
@@ -17,8 +18,7 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = args.debug and False
 app.config["SQLALCHEMY_DATABASE_URI"] = args.database
 app.config['SECRET_KEY'] = 'secret!'
-app.config['EXIT_CODE'] = 1 # 0 = stop server, 1 = fatal error or restart, 2 = update and restart server
-
+app.config['EXIT_CODE'] = 1  # 0 = stop server, 1 = fatal error or restart, 2 = update and restart server
 
 db.init_app(app)
 init_api(app)
@@ -29,6 +29,7 @@ if __name__ == "__main__":
 
     port = (80 if args.debug else 5001) if args.port == -1 else args.port
 
+
     @app.after_request
     def cors_fixer(response):
         response.headers.add("Access-Control-Allow-Origin", "*")
@@ -37,11 +38,17 @@ if __name__ == "__main__":
         return response
 
 
+    @app.errorhandler(InternalServerError)
+    def error_handler(e: InternalServerError):
+        return str(e.original_exception)
+
+
     if args.debug:
         app.run(host="0.0.0.0", port=port)
 
     else:
         Minify(app=app, html=True, js=True, cssless=True)
+
 
         @app.get("/api/stop")
         @admin_only
@@ -49,13 +56,15 @@ if __name__ == "__main__":
             # by default restart the server because we don't want it to have downtime
             app.config['EXIT_CODE'] = int(flask.request.args.get("exit_code", 1))
             exit_reasons = {0: "stop", 1: "restart", 2: "update and restart server", 3: "running test.py"}
-            logger.important(f"User requested server to stop, exit code: {app.config['EXIT_CODE']}: {exit_reasons[app.config['EXIT_CODE']]}\nThe error below is expected and can be ignored")
+            logger.important(
+                f"User requested server to stop, exit code: {app.config['EXIT_CODE']}: {exit_reasons[app.config['EXIT_CODE']]}\nThe error below is expected and can be ignored")
             server.close()
-            return "Stopping server", 200 # there is like a 50% chance this will not be returned, and the server will just close without sending a message to the client. whoopsie
+            return "Stopping server", 200  # there is like a 50% chance this will not be returned, and the server will just close without sending a message to the client. whoopsie
+
 
         logger.info("Starting server...")
         server = create_server(app, host="0.0.0.0", port=port)
         server.run()
 
     logger.info(f"The server has closed, exit code: {app.config['EXIT_CODE']}")
-    exit(app.config['EXIT_CODE']) # return the exit code to the shell
+    exit(app.config['EXIT_CODE'])  # return the exit code to the shell
