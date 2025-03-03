e558a033f0c2c0482670eb030eef43717901a595
redboy1402 <thatgamerdood1402@gmail.com>
2025-03-03 21:58:27 +0800

    waow

---
+++ HandballBackend/Controllers/QOTDController.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HandballBackend.Database.SendableTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandballBackend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class QOTDController : ControllerBase {
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<QuoteOfTheDayData> GetQOTD() {
+        var db = new HandballContext();
+        int today = DateTime.Today.DayOfYear;
+        var quotes = db.QuotesOfTheDay
+            .ToArray();
+        var quote = quotes[today % quotes.Length];
+        return quote.ToSendableData();
+    }
+}
