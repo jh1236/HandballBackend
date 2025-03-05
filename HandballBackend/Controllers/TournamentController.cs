d8b2146f4047f8edd8b8c0cc07d77af6a1bd823b
redboy1402 <thatgamerdood1402@gmail.com>
2025-03-06 03:26:57 +0800

    im nutting into a cantaloupe rn. shit feels so good

---
+++ HandballBackend/Controllers/TournamentController.cs
@@ -0,0 +1,31 @@
+﻿using HandballBackend.Database.SendableTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandballBackend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TournamentController : ControllerBase {
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IEnumerable<TournamentData> GetTournaments() {
+        var db = new HandballContext();
+        var tournaments = db.Tournaments
+            .Select(a => a.ToSendableData())
+            .ToArray();
+        return tournaments;
+    }
+
+    [HttpGet("{searchable}")]
+    public ActionResult<TournamentData> GetTournament(string searchable)
+    {
+        var db = new HandballContext();
+        var tournament = db.Tournaments
+            .FirstOrDefault(a => a.SearchableName == searchable);
+        if (tournament is null)
+        {
+            return NotFound("Invalid Tournament");
+        }
+        return tournament.ToSendableData();
+    }
+}
