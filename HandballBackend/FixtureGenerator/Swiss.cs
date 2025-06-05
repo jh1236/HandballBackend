e458df4a0b2b53ab8fecfa5dc540e68c0b52aaae
jh1236 <63610127+jh1236@users.noreply.github.com>
2025-06-05 13:19:33 +0800

    Add extra Fixture Generators

--- HandballBackend/FixtureGenerator/Swiss.cs
+++ HandballBackend/FixtureGenerator/Swiss.cs
@@ -1,9 +1,11 @@
-﻿using HandballBackend.Database;
+﻿using HandballBackend.Database.Models;
+using HandballBackend.Database.SendableTypes;
+using HandballBackend.EndpointHelpers;
 using HandballBackend.EndpointHelpers.GameManagement;
 
 namespace HandballBackend.FixtureGenerator;
 
-//TODO: Fix this so it works
+//TODO: Test this file; I got lazy and used deepseek
 public class Swiss : AbstractFixtureGenerator {
     private readonly int _tournamentId;
 
@@ -15,8 +17,61 @@ public class Swiss : AbstractFixtureGenerator {
 
     public override bool EndOfRound() {
         var db = new HandballContext();
+
         var tournament = db.Tournaments.Find(_tournamentId)!;
-        var teams = db.TournamentTeams.Where(t => t.TournamentId == _tournamentId).Select(tt => tt.Team).ToList();
-        return base.EndOfRound();
+        var (ladder, _, _) = LadderHelper.GetTournamentLadder(db, tournament);
+
+
+        var currentRound = db.Games
+            .Where(g => g.TournamentId == _tournamentId)
+            .Max(g => (int?) g.Round) ?? 0;
+        var nextRound = currentRound + 1;
+
+        if (nextRound > Math.Ceiling(Math.Log2(ladder!.Length))) {
+            return true;
+        }
+
+        var remainingTeams = new List<TeamData>(ladder);
+        List<(int team1, int team2)> pairings = [];
+
+        while (remainingTeams.Count >= 2) {
+            var topTeam = remainingTeams.First();
+            remainingTeams.Remove(topTeam);
+
+            TeamData? opponent = null;
+            foreach (var potentialOpponent in remainingTeams) {
+                if (HaveTeamsPlayed(db, topTeam.Id, potentialOpponent.Id)) continue;
+                opponent = potentialOpponent;
+                break;
+            }
+
+            opponent ??= remainingTeams.First();
+            remainingTeams.Remove(opponent);
+
+            pairings.Add((topTeam.Id, opponent.Id));
+        }
+
+        if (remainingTeams.Count == 1) {
+            pairings.Add((remainingTeams.First().Id, 1));
+        }
+
+        foreach (var pairing in pairings) {
+            GameManager.CreateGame(
+                tournamentId: _tournamentId,
+                teamOneId: pairing.team1,
+                teamTwoId: pairing.team2,
+                round: nextRound
+            );
+        }
+
+        return false;
+    }
+
+    private bool HaveTeamsPlayed(HandballContext db, int teamOneId, int teamTwoId) {
+        return db.Games.Any(g =>
+            g.TournamentId == _tournamentId &&
+            ((g.TeamOneId == teamOneId && g.TeamTwoId == teamTwoId) ||
+             (g.TeamOneId == teamTwoId && g.TeamTwoId == teamOneId))
+        );
     }
 }
