0441f9db24cc9f843446e41b110f451a8a99d20e
Jared Healy <healy_jared@yahoo.com>
2025-06-12 00:38:20 +0800

    Add sockets

---
+++ HandballBackend/Controllers/ScoreboardController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace HandballBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ScoreboardController : ControllerBase {
+    public Dictionary<int, List<WebSocket>> Sockets = new();
+
+
+
+    public async Task<IActionResult> GetScoreboard(int gameId) {
+        if (!HttpContext.WebSockets.IsWebSocketRequest) {
+            return BadRequest();
+        }
+
+        using var ws = await HttpContext.WebSockets.AcceptWebSocketAsync();
+        return Ok();
+    }
+}
