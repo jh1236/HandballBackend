0b2ecb381e62c1ac9fb0ff838ece89583bb8e2ec
Jared Healy <healy_jared@yahoo.com>
2025-07-14 23:12:07 +0800

    Update Arguments Handler

--- HandballBackend/Arguments/LoggingArgHandler.cs
+++ HandballBackend/Arguments/LoggingArgHandler.cs
@@ -1,7 +1,12 @@
 ﻿namespace HandballBackend.Arguments;
 
-public class LoggingArgHandler() : AbstractArgumentHandler("l", "logRequest", "Logs Requests that the server receives.") {
+public class LoggingArgHandler()
+    : AbstractArgumentHandler("l", "logRequest", "Logs Requests that the server receives.") {
     protected override void ParseIfMatched(string[] args, ref int index, WebApplicationBuilder builder) {
-        Config.REQUEST_LOGGING = true;
+        if (index < args.Length && ((string[]) ["true", "false"]).Contains(args[index])) {
+            Config.REQUEST_LOGGING = args[index++] == "true";
+        } else {
+            Config.REQUEST_LOGGING = true;
+        }
     }
 }
