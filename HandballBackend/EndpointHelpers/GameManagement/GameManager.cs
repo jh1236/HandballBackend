c0b0126782959ab4f2f6e03fa63d6ae15fd665ae
Jared Healy <healy_jared@yahoo.com>
2025-08-11 21:39:02 +0800

    dotnet format

--- HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
+++ HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
@@ -603,11 +603,11 @@ public static class GameManager {
         game.Length = Utilities.GetUnixSeconds() - game.StartTime;
         GameEventSynchroniser.SyncGameEnd(game, endEvent);
         if (!isRandomAbandonment && game is {
-                Ranked:
+            Ranked:
                 true,
-                IsFinal:
+            IsFinal:
                 false
-            }) {
+        }) {
             var playingPlayers = game.Players
                 .Where(pgs => (isForfeit || pgs.RoundsCarded + pgs.RoundsOnCourt > 0)).ToList();
             var teamOneElo = playingPlayers.Where(pgs => pgs.TeamId == game.TeamOneId).Select(pgs => pgs.InitialElo)
@@ -643,7 +643,7 @@ public static class GameManager {
         var db = new HandballContext();
         var teams = new List<Team>();
         var people = await db.People.ToListAsync();
-        foreach (var (players, teamName) in new[] {(playersTeamOne, teamOneName), (playersTeamTwo, teamTwoName)}) {
+        foreach (var (players, teamName) in new[] { (playersTeamOne, teamOneName), (playersTeamTwo, teamTwoName) }) {
             Team team;
             if (players == null || players.Length == 0) {
                 if (teamName == null) {
@@ -706,7 +706,7 @@ public static class GameManager {
         var ranked = tournament.Ranked;
         var isBye = false;
         var tasks = new List<Task>();
-        foreach (var team in new[] {teamOne, teamTwo}) {
+        foreach (var team in new[] { teamOne, teamTwo }) {
             if (team.Id == 1) {
                 // this is the bye team
                 isBye = true;
@@ -794,7 +794,7 @@ public static class GameManager {
             .SingleAsync(); //used to pull extra gamey data
 
         tasks.Clear();
-        foreach (var team in new[] {teamOne, teamTwo}) {
+        foreach (var team in new[] { teamOne, teamTwo }) {
             if (team.Id == 1) continue;
             Person?[] teamPlayers = [team.Captain, team.NonCaptain, team.Substitute];
             foreach (var p in teamPlayers.Where(p => p != null)) {
