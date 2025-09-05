25184436ad2be031ba7f3ebf62088acb421d281c
healy <healy_jared@yahoo.com>
2025-09-05 17:20:05 +0800

    add endpoint for old rules

--- HandballBackend/Controllers/DocumentsController.cs
+++ HandballBackend/Controllers/DocumentsController.cs
@@ -10,16 +10,30 @@ public class DocumentsController : ControllerBase {
     public IActionResult GetRulesFile() {
         return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/rules.pdf"), "application/pdf");
     }
+
     [HttpGet("simplified_rules")]
     public IActionResult GetSimplifiedRulesFile() {
-        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/rules_simple.pdf"), "application/pdf");
+        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/rules_simple.pdf"),
+            "application/pdf");
     }
+
     [HttpGet("code_of_conduct")]
     public IActionResult GetCodeOfConductFile() {
-        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/code_of_conduct.pdf"), "application/pdf");
+        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/code_of_conduct.pdf"),
+            "application/pdf");
     }
+
     [HttpGet("tournament_regulations")]
     public IActionResult GetTournamentRegulationsFile() {
-        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/tournament_regulations.pdf"), "application/pdf");
+        return File(System.IO.File.OpenRead(Config.RESOURCES_FOLDER + "/documents/pdf/tournament_regulations.pdf"),
+            "application/pdf");
     }
+
+    [HttpGet("rules/{tournament}")]
+    public IActionResult GetOldRulesFile(string tournament) {
+        var fileName = Uri.EscapeDataString(tournament);
+        var path = Config.RESOURCES_FOLDER + "/documents/old/" + tournament + ".pdf";
+        return File(System.IO.File.OpenRead(path + fileName + ".png"), "image/png");
+    }
+
 }
