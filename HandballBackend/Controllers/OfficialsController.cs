4a26fbc981af3b7ff5ffe180b9022496d555ff97
healy <healy_jared@yahoo.com>
2025-09-19 11:20:15 +0800

    move official tournament endpoints

--- HandballBackend/Controllers/OfficialsController.cs
+++ HandballBackend/Controllers/OfficialsController.cs
@@ -98,4 +98,82 @@ public class OfficialsController : ControllerBase {
             tournament = returnTournament ? tournament!.ToSendableData() : null
         };
     }
+    
+    
+    public class AddOfficialRequest {
+        public required string OfficialSearchableName { get; set; }
+        public required string TournamentSearchableName { get; set; }
+    }
+
+    [HttpPost("addOfficialToTournament")]
+    [TournamentAuthorize(PermissionType.UmpireManager)]
+    public async Task<ActionResult> AddOfficialToTournament(
+        [FromBody] AddOfficialRequest request) {
+        var db = new HandballContext();
+        var tournament = await db.Tournaments
+            .FirstOrDefaultAsync(a => a.SearchableName == request.TournamentSearchableName);
+        if (tournament is null) {
+            return NotFound("Invalid Tournament");
+        }
+
+        if (tournament.Started) {
+            return NotFound("Tournament has already started!");
+        }
+
+        var official = await db.Officials.IncludeRelevant().Include(official => official.TournamentOfficials)
+            .FirstOrDefaultAsync(o => o.Person.SearchableName == request.OfficialSearchableName);
+
+
+        if (official == null) {
+            return BadRequest("The Official doesn't exist");
+        }
+
+        if (official.TournamentOfficials.Any(to => to.TournamentId == tournament.Id)) {
+            return BadRequest("That official is already in this tournament!");
+        }
+
+        await db.TournamentOfficials.AddAsync(new TournamentOfficial {
+            TournamentId = tournament.Id,
+            OfficialId = official.Id,
+            Role = OfficialRole.Umpire,
+            UmpireProficiency = official.Proficiency,
+            ScorerProficiency = official.Proficiency,
+        });
+
+        await db.SaveChangesAsync();
+        return Ok();
+    }
+
+    public class RemoveOfficialRequest {
+        public required string OfficialSearchableName { get; set; }
+        public required string TournamentSearchableName { get; set; }
+    }
+
+    [TournamentAuthorize(PermissionType.UmpireManager)]
+    [HttpDelete("removeOfficialFromTournament")]
+    public async Task<ActionResult> RemoveOfficialFromTournament([FromBody] RemoveOfficialRequest request) {
+        var db = new HandballContext();
+        var tournament = await db.Tournaments
+            .FirstOrDefaultAsync(a => a.SearchableName == request.TournamentSearchableName);
+        if (tournament is null) {
+            return NotFound("Invalid Tournament");
+        }
+
+        if (tournament.Started) {
+            return NotFound("Tournament has already started!");
+        }
+
+        var tournamentOfficial = await db.TournamentOfficials.FirstOrDefaultAsync(to =>
+            to.TournamentId == tournament.Id && to.Official.Person.SearchableName == request.OfficialSearchableName);
+
+
+        if (tournamentOfficial == null) {
+            return BadRequest("The Official doesn't exist");
+        }
+
+        db.TournamentOfficials.Remove(tournamentOfficial);
+
+        await db.SaveChangesAsync();
+        return Ok();
+    }
 }
