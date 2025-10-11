5aae0f1827e97dd00ee6ed9032748f2d8a76de9e
Jared Healy <healy_jared@yahoo.com>
2025-10-12 00:55:00 +0800

    Allows for easy creation of solo teams

--- HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
+++ HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
@@ -640,11 +640,11 @@ public static class GameManager {
         game.Length = Utilities.GetUnixSeconds() - game.StartTime;
         GameEventSynchroniser.SyncGameEnd(game, endEvent);
         if (!isRandomAbandonment && game is {
-            Ranked:
+                Ranked:
                 true,
-            IsFinal:
+                IsFinal:
                 false
-        }) {
+            }) {
             var playingPlayers = game.Players
                 .Where(pgs => (isForfeit || pgs.RoundsCarded + pgs.RoundsOnCourt > 0)).ToList();
             var teamOneElo = playingPlayers.Where(pgs => pgs.TeamId == game.TeamOneId).Select(pgs => pgs.InitialElo)
@@ -683,7 +683,8 @@ public static class GameManager {
         var teams = new List<Team>();
         var people = await
             db.People.Where(p => allNames.Contains(p.Name)).ToListAsync();
-        foreach (var (players, teamName) in new[] { (playersTeamOne, teamOneName), (playersTeamTwo, teamTwoName) }) {
+        foreach (var (players, givenTeamName) in new[] {(playersTeamOne, teamOneName), (playersTeamTwo, teamTwoName)}) {
+            var teamName = givenTeamName;
             Team team;
             if (players == null || players.Length == 0) {
                 if (teamName == null) {
@@ -711,6 +712,14 @@ public static class GameManager {
                         (t.SubstituteId.HasValue ? 1 : 0) == playerIds.Count(a => a.HasValue))
                 );
                 if (maybeTeam == null) {
+                    if (playerIds[1] == null) {
+                        teamName = "(Solo) " + players[0];
+                    }
+
+                    if (teamName == null) {
+                        throw new InvalidOperationException("Must pass a team name when creating a team!");
+                    }
+
                     team = new Team {
                         CaptainId = playerIds![0],
                         NonCaptainId = playerIds[1],
@@ -718,6 +727,10 @@ public static class GameManager {
                         Name = teamName,
                         SearchableName = Utilities.ToSearchable(teamName)
                     };
+                    if (playerIds[1] == null) {
+                        team.TeamColor = "#12114a";
+                    }
+
                     _ = Task.Run(() => ImageHelper.SetGoogleImageForTeam(team.Id));
                     await db.Teams.AddAsync(team);
                 } else {
@@ -747,7 +760,7 @@ public static class GameManager {
         var ranked = tournament.Ranked;
         var isBye = false;
         var tasks = new List<Task>();
-        foreach (var team in new[] { teamOne, teamTwo }) {
+        foreach (var team in new[] {teamOne, teamTwo}) {
             if (team.Id == 1) {
                 // this is the bye team
                 isBye = true;
@@ -832,7 +845,7 @@ public static class GameManager {
             .ToDictionaryAsync(pgs => pgs!.PlayerId);
 
         tasks.Clear();
-        foreach (var team in new[] { teamOne, teamTwo }) {
+        foreach (var team in new[] {teamOne, teamTwo}) {
             if (team.Id == 1) continue;
             Person?[] teamPlayers = [team.Captain, team.NonCaptain, team.Substitute];
             foreach (var p in teamPlayers.Where(p => p != null).Cast<Person>()) {
