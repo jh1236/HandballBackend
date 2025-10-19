fca1a02bfb518ca9fd7c7660eb6cbcd68e428e1d
Jared Healy <healy_jared@yahoo.com>
2025-10-19 13:43:28 +0800

    rename to gameId

--- HandballBackend/Database/SendableTypes/GameEventData.cs
+++ HandballBackend/Database/SendableTypes/GameEventData.cs
@@ -6,7 +6,7 @@ namespace HandballBackend.Database.SendableTypes;
 public class GameEventData {
     public int Id { get; private set; }
     
-    public int GameNumber { get; private set; }
+    public int GameId { get; private set; }
     public GameEventType EventType { get; private set; }
     public bool? FirstTeam { get; private set; }
     public PersonData? Player { get; private set; }
@@ -35,7 +35,7 @@ public class GameEventData {
         FirstTeamToServe = gameEvent.TeamToServeId == teamOneId;
         SideToServe = gameEvent.SideToServe;
         Time = gameEvent.CreatedAt > 0 ? gameEvent.CreatedAt : null;
-        GameNumber = gameEvent.Game.GameNumber;
+        GameId = gameEvent.Game.GameNumber;
         if (includeGame) {
             Game = gameEvent.Game.ToSendableData();
         }
