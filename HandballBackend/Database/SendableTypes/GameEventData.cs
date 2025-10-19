f003eba468a0ef4ed7d17f9782bece47845508ba
Jared Healy <healy_jared@yahoo.com>
2025-10-19 13:40:18 +0800

    provides game number with events

--- HandballBackend/Database/SendableTypes/GameEventData.cs
+++ HandballBackend/Database/SendableTypes/GameEventData.cs
@@ -5,6 +5,8 @@ namespace HandballBackend.Database.SendableTypes;
 
 public class GameEventData {
     public int Id { get; private set; }
+    
+    public int GameNumber { get; private set; }
     public GameEventType EventType { get; private set; }
     public bool? FirstTeam { get; private set; }
     public PersonData? Player { get; private set; }
@@ -33,6 +35,7 @@ public class GameEventData {
         FirstTeamToServe = gameEvent.TeamToServeId == teamOneId;
         SideToServe = gameEvent.SideToServe;
         Time = gameEvent.CreatedAt > 0 ? gameEvent.CreatedAt : null;
+        GameNumber = gameEvent.Game.GameNumber;
         if (includeGame) {
             Game = gameEvent.Game.ToSendableData();
         }
