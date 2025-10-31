ef67555e03bda82f8154a38f0b0c0fd0d3898810
Jared Healy <healy_jared@yahoo.com>
2025-10-31 12:49:22 +0800

    format

--- HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
+++ HandballBackend/EndpointHelpers/GameManagement/GameManager.cs
@@ -684,13 +684,13 @@ public static class GameManager {
         game.Length = Utilities.GetUnixSeconds() - game.StartTime;
         GameEventSynchroniser.SyncGameEnd(game, endEvent);
         if (!isRandomAbandonment && game is {
-                Ranked:
+            Ranked:
                 true,
-                IsFinal:
+            IsFinal:
                 false,
-                TeamOne.NonCaptainId: not null,
-                TeamTwo.NonCaptain: not null
-            }) {
+            TeamOne.NonCaptainId: not null,
+            TeamTwo.NonCaptain: not null
+        }) {
             var playingPlayers = game.Players
                 .Where(pgs => (isForfeit || pgs.RoundsCarded + pgs.RoundsOnCourt > 0)).ToList();
             var playingPlayerIds = playingPlayers.Select(pgs => pgs.PlayerId).ToList();
@@ -821,7 +821,7 @@ public static class GameManager {
         var ranked = tournament.Ranked;
         var isBye = false;
         var tasks = new List<Task>();
-        foreach (var team in new[] {teamOne, teamTwo}) {
+        foreach (var team in new[] { teamOne, teamTwo }) {
             if (team.Id == 1) {
                 // this is the bye team
                 isBye = true;
@@ -906,7 +906,7 @@ public static class GameManager {
             .ToDictionaryAsync(pgs => pgs!.PlayerId);
 
         tasks.Clear();
-        foreach (var team in new[] {teamOne, teamTwo}) {
+        foreach (var team in new[] { teamOne, teamTwo }) {
             if (team.Id == 1) continue;
             Person?[] teamPlayers = [team.Captain, team.NonCaptain, team.Substitute];
             foreach (var p in teamPlayers.Where(p => p != null).Cast<Person>()) {
